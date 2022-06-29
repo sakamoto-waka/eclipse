abf638cd888874cfde2adfa3e859675e8bd7e96e
sakamoto-waka <wakanmhl@gmail.com>
2022-06-29 21:21:38 +0900

    [update] chapter15_2

--- src/practice/Chapter15_2.java
+++ src/practice/Chapter15_2.java
@@ -1,5 +1,14 @@
 package practice;
 
-public class Chapter15_2 {
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
+
+public class Chapter15_2 {
+	public static void main(String [] args) {
+		FileSystem fs = FileSystems.getDefault();
+		Path inputPath = fs.getPath("resources/input.txt");
+		Path outputPath = fs.getPath("resources/output.txt");
+	}
 }
