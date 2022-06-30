1fe5b869e950e386aafc356a8dd123cebb5a52bf
sakamoto-waka <wakanmhl@gmail.com>
2022-06-30 22:53:42 +0900

    [finish] advance_practice01

---
+++ src/AdvancePractice1.java
@@ -0,0 +1,8 @@
+
+public class AdvancePractice1 {
+	public class HumanLeg {
+		public void kick() {
+			System.out.println("蹴る");
+		}
+	}
+}
---
+++ src/AdvancePracticeExecution.java
@@ -0,0 +1,10 @@
+
+public class AdvancePracticeExecution {
+	public static void main(String[] args) {
+		AdvancePractice1 ap1 = new AdvancePractice1();
+		AdvancePractice1.HumanLeg leg = ap1.new HumanLeg();
+		leg.kick();
+		
+	}
+
+}
