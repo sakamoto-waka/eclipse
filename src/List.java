a849c01936c0b7c78b986dd59acffd2c9736d214
sakamoto-waka <wakanmhl@gmail.com>
2022-07-10 11:28:42 +0900

    [start] ad-7

---
+++ src/List.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+
+public class List implements Comparable<List> {
+	private LocalDate date;
+	private String list;
+	private boolean done;
+
+	public List(LocalDate date, String list, boolean done) {
+		this.date = date;
+		this.list = list;
+		this.done = done;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	@Override
+	public int compareTo(List t) {
+		return date.compareTo(t.date);
+	}
+
+	@Override
+	public String toString() {
+		return date + ":" + list + ":" + done + "]";
+	}
+}
